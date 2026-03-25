bb7a8d6d0e184f0c7511b7a6a74e4c83789c0011
Victor Alves Bugueno <victor.bugueno@fiap.com.br>
2026-03-25 00:34:58 -0300

    Refatorada exibição de lista de planos ativos e finalizados, no menu lateral

--- src/components/side-drawer.tsx
+++ src/components/side-drawer.tsx
@@ -23,7 +23,7 @@ function TextBalanceForFinishedPlan({ balance }: TextBalanceProps) {
   const formattedBalance = `${signal}${formatMoney(Math.abs(balance))}`;
   return (
     <Text
-      style={{color: getColor(isPositive ? "lime" : "danger")}} className={`font-semibold`}
+      style={{ color: getColor(isPositive ? "lime" : "danger") }} className={`font-semibold`}
     >
       {formattedBalance}
     </Text>
@@ -50,7 +50,7 @@ export default function SideDrawer({ isOpen, onClose }: SideDrawerProps) {
   useEffect(() => {
     const fetchUser = async () => {
       const dbUser = await userRepository.getById('ad50726d466b4f59937f210c5344188d');
-      console.log({dbUser});
+      console.log({ dbUser });
       setUser(dbUser)
     };
 
@@ -77,12 +77,12 @@ export default function SideDrawer({ isOpen, onClose }: SideDrawerProps) {
         className={`flex flex-col w-[80%] bg-white transition-all duration-500 ${isOpen ? 'right-0' : 'right-[100%]'}`}
       >
         <LinearGradient
-                  colors={[getColor("violet"), getColor("purple")]}
+          colors={[getColor("violet"), getColor("purple")]}
           start={{ x: 0, y: 0.5 }}
           end={{ x: 1, y: 0.5 }}
           className="flex flex-row items-center justify-between gap-3 px-6 pt-6 pb-10"
         >
-          <View style={{backgroundColor: getColor("light-violet")}} className="flex items-center justify-center w-12 h-12 rounded-full">
+          <View style={{ backgroundColor: getColor("light-violet") }} className="flex items-center justify-center w-12 h-12 rounded-full">
             <FontAwesome6 name="trophy" size={20} color={getColor("gold")} />
           </View>
           <View className="flex flex-col items-start justify-center flex-1">
@@ -99,53 +99,58 @@ export default function SideDrawer({ isOpen, onClose }: SideDrawerProps) {
           </Pressable>
         </LinearGradient>
         <View className="flex flex-col">
-          <View className="flex flex-col">
-            <Text style={{color: getColor("gray-7")}} className="px-6 pt-4 pb-2 text-xs font-semibold uppercase">
-              Planos Ativos
-            </Text>
-            {activePlans.map((plan) => (
-              <Pressable
-                key={plan.id}
-                className="flex flex-row items-center justify-start gap-3 px-8 py-4"
-                onPress={async () => {
-                  await asyncStorageRepository.set('planId', plan.id);
-                  router.push('/plan-tracker');
-                  onClose();
-                }}
-              >
-                <View style={{backgroundColor: getColor("lime")}} className="w-2 h-2 rounded-full"></View>
-                <Text style={{color: getColor("gray-3")}} className="font-semibold">{plan.description ?? plan.habitName}</Text>
-              </Pressable>
-            ))}
-          </View>
-          <View className="flex flex-col">
-            <Text style={{color: getColor("gray-7")}} className="px-6 pt-4 pb-2 text-xs font-semibold uppercase">
-              Planos Finalizados
-            </Text>
-            {finishedPlans.map((plan) => (
-              <View
-                key={plan.id}
-                className="flex flex-row items-center justify-between gap-3 px-8 py-4"
-              >
-                <Text style={{color: getColor("gray-3")}} className="font-semibold">{plan.description ?? plan.habitName}</Text>
-                <TextBalanceForFinishedPlan balance={/*plan.balance*/ 123} />
-              </View>
-            ))}
-          </View>
-          <View style={{backgroundColor: getColor("gray-9"), width: "90%", height: 0.5}} className="mx-auto my-6"></View>
-          <View className="flex flex-row items-center justify-start gap-3 px-8 py-3">
-            <View className="flex flex-row items-center justify-center w-5">
-              <Ionicons name="home-outline" size={16} color={getColor("gray-3")} />
+          {
+            activePlans.length > 0
+            && <View className="flex flex-col">
+              <Text style={{ color: getColor("gray-7") }} className="px-6 pt-4 pb-2 text-xs font-semibold uppercase">
+                Planos Ativos
+              </Text>
+              {
+                activePlans.map((plan) => (
+                  <Pressable
+                    key={plan.id}
+                    className="flex flex-row items-center justify-start gap-3 px-8 py-4"
+                    onPress={async () => {
+                      await asyncStorageRepository.set('planId', plan.id);
+                      router.push('/plan-tracker');
+                      onClose();
+                    }}
+                  >
+                    <View style={{ backgroundColor: getColor("lime") }} className="w-2 h-2 rounded-full"></View>
+                    <Text style={{ color: getColor("gray-3") }} className="font-semibold">{plan.description ?? plan.habitName}</Text>
+                  </Pressable>
+                ))
+              }
             </View>
-            <Text style={{color: getColor("gray-3")}} className="font-semibold">Início</Text>
-          </View>
+          }
+          {
+            finishedPlans.length > 0
+            && <View className="flex flex-col">
+              <Text style={{ color: getColor("gray-7") }} className="px-6 pt-4 pb-2 text-xs font-semibold uppercase">
+                Planos Finalizados
+              </Text>
+              {finishedPlans.map((plan) => (
+                <View
+                  key={plan.id}
+                  className="flex flex-row items-center justify-between gap-3 px-8 py-4"
+                >
+                  <Text style={{ color: getColor("gray-3") }} className="font-semibold">{plan.description ?? plan.habitName}</Text>
+                  <TextBalanceForFinishedPlan balance={/*plan.balance*/ 123} />
+                </View>
+              ))}
+            </View>
+          }
+          {
+            plans.length > 0 
+            && <View style={{ backgroundColor: getColor("gray-9"), width: "90%", height: 0.5 }} className="mx-auto my-6"></View>
+            }
           <Pressable
-            onPress={() => {router.replace('/manage-plans'); onClose();}}
-             className="flex flex-row items-center justify-start gap-3 px-8 py-3">
+            onPress={() => { router.replace('/manage-plans'); onClose(); }}
+            className="flex flex-row items-center justify-start gap-3 px-8 py-3">
             <View className="flex flex-row items-center justify-center w-5">
               <Ionicons name="add-circle-outline" size={16} color={getColor("gray-3")} />
             </View>
-            <Text style={{color: getColor("gray-3")}} className="font-semibold">
+            <Text style={{ color: getColor("gray-3") }} className="font-semibold">
               Gerenciar Planos
             </Text>
           </Pressable>
@@ -153,7 +158,7 @@ export default function SideDrawer({ isOpen, onClose }: SideDrawerProps) {
             <View className="flex flex-row items-center justify-center w-5">
               <Ionicons name="settings-outline" size={16} color={getColor("gray-3")} />
             </View>
-            <Text style={{color: getColor("gray-3")}} className="my-auto font-semibold">
+            <Text style={{ color: getColor("gray-3") }} className="my-auto font-semibold">
               Configurações
             </Text>
           </View>
@@ -165,7 +170,7 @@ export default function SideDrawer({ isOpen, onClose }: SideDrawerProps) {
                 color={getColor("gray-3")}
               />
             </View>
-            <Text style={{color: getColor("gray-3")}} className="font-semibold">Sobre nós</Text>
+            <Text style={{ color: getColor("gray-3") }} className="font-semibold">Sobre nós</Text>
           </View>
           <Pressable
             onPress={() => router.replace('/login')}
@@ -174,7 +179,7 @@ export default function SideDrawer({ isOpen, onClose }: SideDrawerProps) {
             <View className="flex flex-row items-center justify-center w-5">
               <Ionicons name="log-out-outline" size={16} color={getColor("danger")} />
             </View>
-            <Text style={{color: getColor("danger")}} className="font-semibold">Sair</Text>
+            <Text style={{ color: getColor("danger") }} className="font-semibold">Sair</Text>
           </Pressable>
         </View>
       </View>
